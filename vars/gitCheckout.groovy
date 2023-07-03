722f44d58de510ef14cab1a34c73055589842e91
unknown <sirine.JNAYEH@esprit.tn>
2023-07-03 18:15:34 +0100

    fixing typos

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -2,7 +2,7 @@ def call(map stageParams){
     checkout(
         [$class: 'GitSCM',
         branches: [[name: stageParams.branch]],
-        userRemoteCongigs=[[url:stageParams.url]]
+        userRemoteConfigs: [[url:stageParams.url]]
 
         ]
     )
