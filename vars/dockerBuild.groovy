6bfc90393c04cae9206cbb5238aeab3c0bef67b8
unknown <sirine.JNAYEH@esprit.tn>
2023-07-06 20:43:16 +0100

    tp

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(String project,String imagetag, String dockerHubUser){
     sh """
-    docker build -t ${dockerHubUser}/${project}
+    docker image build -t ${dockerHubUser}/${project}
     docker image tag ${dockerHubUser}/${project} ${dockerHubUser}/${project}:${imagetag}
     docker image tag ${dockerHubUser}/${project} ${dockerHubUser}/${project}:latest
     """
