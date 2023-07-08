9f874ae7e30fcf5fb13e044d4cba2074f9f70fde
unknown <sirine.JNAYEH@esprit.tn>
2023-07-08 13:53:51 +0100

    dockerpush.groovy

---
+++ vars/dockerpush.groovy
@@ -0,0 +1,11 @@
+def call(String project,String imagetag, String dockerHubUser){
+    withCredentials([usernamePassword(credentialsId: 'dockerhub',
+                                      passwordVariable: 'PASS', 
+                                      usernameVariable: 'USER')]) {
+    sh 'docker login -u '$USER' -p '$PASS''
+}
+sh """
+docker image push ${dockerHubUser}/${project}:${imagetag}
+docker image push ${dockerHubUser}/${project}:latest
+"""
+}
