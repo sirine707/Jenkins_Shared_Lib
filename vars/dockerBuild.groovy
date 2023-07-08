c02ddc33684b5da831418d106ca046ee0a60d128
unknown <sirine.JNAYEH@esprit.tn>
2023-07-08 20:09:43 +0100

    added

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -5,9 +5,9 @@
     docker image tag ${dockerHubUser}/${project} ${dockerHubUser}/${project}:latest
     """
 } */
-def call(String aws_account_id,String region,String repoName){
-    sh"""
-    docker build -t ${repoName} .
-    docker tag ${repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${repoName}:latest
+def call(String aws_account_id,String region,String ecr_repoName){
+    sh """
+     docker build -t ${ecr_repoName} .
+     docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
