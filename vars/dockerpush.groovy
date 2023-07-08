2ac375d1c2046c3ab68992e48cb90990fb86892e
unknown <sirine.JNAYEH@esprit.tn>
2023-07-08 14:09:06 +0100

    ftypos

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -2,7 +2,7 @@ def call(String project,String imagetag, String dockerHubUser){
     withCredentials([usernamePassword(credentialsId: 'dockerhub',
                                       passwordVariable: 'PASS', 
                                       usernameVariable: 'USER')]) {
-    sh 'docker login -u '$USER' -p '$PASS''
+    sh "docker login -u '$USER' -p '$PASS'"
 }
 sh """
 docker image push ${dockerHubUser}/${project}:${imagetag}
